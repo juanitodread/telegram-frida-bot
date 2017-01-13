34c869813a8f583be8585f72bfe08bc78c876802
juanitodread <juanitodread@gmail.com>
2017-01-13 17:58:45 -0600

    Add basic giphy functionality

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
-const dotenv   = require('dotenv').config();
+require('dotenv').config();
 const Telegraf = require('telegraf');
+const GiphyWrapper = require('./lib/GiphyWrapper');
 
 const app = new Telegraf(process.env.BOT_TOKEN);
 
-app.command('start', (ctx) => {
+app.command('start', ctx => {
   console.log('start', ctx.form);
-  ctx.reply('Welcome, my name is JuanBot but currently Im using a Frida :D profile picture');
+  ctx.reply('Hola, soy FridaBot, qué quieres que busque?');
 });
 
-app.hears('hi', (ctx) => ctx.reply('Hey there!'));
+app.hears('hi', ctx => ctx.reply('Hey there!'));
 
-app.on('sticker', (ctx) => ctx.reply('👍'));
+app.on('sticker', ctx => ctx.reply('👍'));
 
 const msg = '...\n\nI will be giving you a smarter answer when my creator improve me :D - FridaBot';
 
-app.on('message', (ctx) => {
-  console.log(ctx.message);
-  ctx.message.text += msg;
-  console.log(ctx.message);
-  ctx.telegram.sendCopy(ctx.from.id, ctx.message);
+app.on('message', ctx => {
+  const queryMsg = ctx.message.text;
+
+  if(queryMsg.toLowerCase().startsWith("frida busca")) {
+    const realMsg = ctx.message.text.toLowerCase().replace("frida busca", "");
+    ctx.telegram.sendCopy(ctx.from.id, ctx.message);
+
+    console.log(`IMAGEeeeee: ${GiphyWrapper.findImage(realMsg)}`);
+
+    GiphyWrapper.findImage(realMsg).then(res => {
+      console.log('Entrando a findImage promise');
+      //console.log(res);
+      //console.log(`Images found: ${JSON.stringify(res.data[0].images)}`);
+      //console.log(res.data[0].images.looping.mp4);
+      const imgUrl = res.data[0].images.looping.mp4;
+      console.log(`Image to RETURN: ${imgUrl}`);
+      console.log("Context: " + ctx);
+      ctx.replyWithVideo({
+        url: imgUrl
+      });
+    }).catch(err => {
+      console.log("Error: " + err);
+      return err;
+    });
+  } else {
+      ctx.reply("Prueba buscando algo escribiendo: frida busca");
+  }
+
 });
 
 app.startPolling();
