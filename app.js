ffb7086a6eef09c117bd3d0a9b0ed901c81aa468
juanitodread <juanitodread@gmail.com>
2017-01-17 16:53:39 -0600

    Refactor the code to use an input handler to delegate the response according to the incoming messages

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Telegraf = require('telegraf');
-const GiphyWrapper = require('./lib/GiphyWrapper');
 const Util = require('./lib/Util');
+const TIH = require('./lib/TelegramInputHandler');
 
 const app = new Telegraf(process.env.BOT_TOKEN);
 const Logger = Util.getLogger();
@@ -11,35 +11,11 @@ app.command('start', ctx => {
   ctx.reply('Hola, soy FridaBot, qué quieres que busque?');
 });
 
-app.hears('hi', ctx => ctx.reply('Hey there!'));
+app.hears('hi', ctx => ctx.reply('Hola amigo!'));
 
 app.on('sticker', ctx => ctx.reply('👍'));
 
-app.on('message', ctx => {
-  const queryMsg = ctx.message.text;
-
-  if (queryMsg.toLowerCase().startsWith('frida busca')) {
-    const realMsg = ctx.message.text.toLowerCase().replace('frida busca', '');
-    ctx.telegram.sendCopy(ctx.from.id, ctx.message);
-
-    Logger.debug(`IMAGEeeeee: ${GiphyWrapper.findImage(realMsg)}`);
-
-    GiphyWrapper.findImage(realMsg).then(res => {
-      Logger.info('Entrando a findImage promise');
-      const imgUrl = res.data[0].images.looping.mp4;
-      Logger.debug(`Image to RETURN: ${imgUrl}`);
-      Logger.debug(`Context: ${ctx}`);
-      ctx.replyWithVideo({
-        url: imgUrl
-      });
-    }).catch(err => {
-      Logger.error(`Error: ${err}`);
-      return err;
-    });
-  } else {
-    ctx.reply('Prueba buscando algo escribiendo: frida busca');
-  }
-
-});
+// On messages process them with TelegramInputHandler
+app.on('message', TIH.onMessage);
 
 app.startPolling();
